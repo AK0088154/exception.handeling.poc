8b51d27dedb93999e204db42a8f2faee7aff69f7
Aditya Kumar <AK0088154@techmahindra.com>
2017-11-24 01:45:17 +0530

    update(poc): fine tuned all the different js and html

--- js/about.js
+++ js/about.js
@@ -1,22 +1,8 @@
 var scotchApp = angular.module('scotchApp');
 scotchApp.controller("aboutController", function($scope, $exceptionHandler) {
     $scope.message = 'About Page - set using aboutController';
-    var onSuccess = function(response) {
-        $scope.status = response.status;
-        $scope.data = response.data;
-    };
 
-    var onError = function(response) {
-        $scope.status = response.status;
-        $scope.data = response.data;
-        $scope.error == "Name already in use"
-
-    }
-    $scope.getException = function() {
-        $http.get("/getdata").then(onSuccess, onError);
-    };
-
-    $scope.throwExpt = function() {
-        throw new Error("aboutController : An Error Occured");
+    $scope.throwException = function() {
+        throw new Error("aboutController : An Error Occured2");
     }
 });
--- js/contact.js
+++ js/contact.js
@@ -1,21 +1,13 @@
 var scotchApp = angular.module('scotchApp');
 scotchApp.controller("contactController", function($scope, $exceptionHandler) {
     $scope.message = 'Contact Page - set using contactController';
-    var onSuccess = function(response) {
-        $scope.status = response.status;
-        $scope.data = response.data;
-    };
 
-    var onError = function(response) {
-        $scope.status = response.status;
-        $scope.data = response.data;
-    }
-
-    $scope.getStudent = function() {
-        $http.get("/getdata").then(onSuccess, onError);
-    };
-
-    $scope.setException = function() {
-		throw new Error("Contact Controller: An Error Occured3");
+    $scope.reThrowException = function() {
+			try{
+					throw new Error("Contact Controller: An Error Occured3");
+			}
+			catch(except){
+					$exceptionHandler(except, " : Button was Pressed2");
+			}
     }
 });
